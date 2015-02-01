fb97fa67f446c71ce26fda2b441100d4a7f774bc
David Lilue <dvdalilue@gmail.com>
2015-02-01 12:32:19 -0430

    Visualizacion de la ola. Amplitud y Frecuencia
    Se puede cambiar la amplitud y frecuencia por teclado

--- OlaCircular/main.cpp
+++ OlaCircular/main.cpp
@@ -2,41 +2,350 @@
 #include <GL\freeglut.h>
 #include <iostream>
 
-void DrawCircle(float cx, float cy, float r, int num_segments) 
-{ 
-	glBegin(GL_LINE_LOOP); 
-		for(int i = 0; i < num_segments; i++) 
-		{ 
-			float theta = 2.0f * 3.1415926f * float(i) / float(num_segments);
+using namespace std;
 
-			float x = r * cosf(theta);
-			float y = r * sinf(theta);
+#define DEF_floorGridScale	1.0
+#define DEF_floorGridXSteps	10.0
+#define DEF_floorGridZSteps	10.0
 
-			glVertex2f(x + cx, y + cy);
-		} 
-	glEnd(); 
+int angle = 90,
+	Xpos = 10,
+	Ypos = 10,
+	k = 1;
+double Zpos = 3.0,
+		t = 0.0;
+
+float amplitud = 1.0,
+	frecuencia = 1.0;
+
+GLfloat ctlpoints[21][21][3];
+
+GLUnurbsObj *theNurb;
+
+void ejesCoordenada() {
+	
+	glLineWidth(2.5);
+	glBegin(GL_LINES);
+		glColor3f(1.0,0.0,0.0);
+		glVertex2f(0,10);
+		glVertex2f(0,-10);
+		glColor3f(0.0,0.0,1.0);
+		glVertex2f(10,0);
+		glVertex2f(-10,0);
+	glEnd();
+
+	glLineWidth(1.5);
+	int i;
+	glColor3f(0.0,1.0,0.0);
+	glBegin(GL_LINES);
+		for(i = -10; i <=10; i++){
+			if (i!=0) {		
+				if ((i%2)==0){	
+					glVertex2f(i,0.4);
+					glVertex2f(i,-0.4);
+
+					glVertex2f(0.4,i);
+					glVertex2f(-0.4,i);
+				}else{
+					glVertex2f(i,0.2);
+					glVertex2f(i,-0.2);
+
+					glVertex2f(0.2,i);
+					glVertex2f(-0.2,i);
+
+				}
+			}
+		}
+		
+	glEnd();
+
+	glLineWidth(1.0);
 }
 
-void render()
-{
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+void changeViewport(int w, int h) {
 	
-	DrawCircle(0.0f, 0.0f, 0.5f, 25);
+	float aspectratio;
 
-	glutSwapBuffers( ); 
+	if (h==0)
+		h=1;
+
+	
+   glViewport (0, 0, (GLsizei) w, (GLsizei) h); 
+   glMatrixMode (GL_PROJECTION);
+   glLoadIdentity ();
+   gluPerspective(30, (GLfloat) w/(GLfloat) h, 1.0, 200.0);
+   glMatrixMode (GL_MODELVIEW);
+
+}
+
+float direction(int x, int y, int cx, int cy) {
+	float l = sqrt(((x - cx)*(x - cx)) + ((y - cy)*(y - cy)));
+	return l;
+}
+
+void init_surface() {
+	int u, v;
+
+    for (u = 0; u < 21; u++) {
+		for (v = 0; v < 21; v++) {
+			ctlpoints[u][v][0] = ((GLfloat)u - 10.0f);
+			ctlpoints[u][v][1] = ((GLfloat)v - 10.0f);
+			ctlpoints[u][v][2] = amplitud*(pow(sin(direction(u,v,Xpos,Ypos)*frecuencia - t), k));
+		}
+    }
+}
+
+void init(){
+
+	//GLfloat mat_diffuse[] = { 0.7, 0.7, 0.7, 1.0 };
+    //GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+    //GLfloat mat_shininess[] = { 100.0 };
+
+    //glClearColor (0.0, 0.0, 0.0, 1.0);
+    //glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+    //glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+    //glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	//glDepthFunc(GL_LESS);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_AUTO_NORMAL);
+	glEnable(GL_NORMALIZE);
+
+	init_surface();
+
+	theNurb = gluNewNurbsRenderer();
+	gluNurbsProperty(theNurb, GLU_SAMPLING_TOLERANCE, 15.0); //25.0
+	gluNurbsProperty(theNurb, GLU_DISPLAY_MODE, GLU_FILL);
+}
+
+void Keyboard(unsigned char key, int x, int y)
+{
+  switch (key)
+  {
+	case 27:             
+		exit (0);
+		break;
+	case 'a':
+		amplitud += 0.1;
+		break;
+	case 'q':
+		amplitud -= 0.1;
+		break;
+	case 's':
+		frecuencia += 0.1;
+		break;
+	case 'w':
+		frecuencia -= 0.1;
+		break;
+	case 'd':
+		k += 1;
+		break;
+	case 'e':
+		if (k > 1)
+			k -= 1;
+		break;
+  }
+}
+
+void specialKey (int key, int x, int y) {
+	switch (key)
+	{
+		case GLUT_KEY_UP:
+			Xpos += 1;
+			break;
+		case GLUT_KEY_DOWN:
+			Xpos -= 1;
+			break;
+		case GLUT_KEY_LEFT:
+			Ypos -= 1;
+			break;
+		case GLUT_KEY_RIGHT:
+			Ypos += 1;
+			break;
+	}	
+}
+
+GLfloat * knotVector(int k) {
+	static GLfloat r[25];
+
+	for (int i = 0; i < k; ++i) {
+		r[i] = 0.0f;
+	}
+
+	for (int i = k; i < (25-k); ++i) {
+		r[i] = float(i-3)/18.0;
+	}
+
+	for (int i = (25-k); i < 25; ++i) {
+		r[i] = 1.0f;
+	}
+
+	return r;
+}
+
+void curva() {
+	
+	GLfloat *knots;
+
+	knots = knotVector(4);
+
+	gluBeginSurface(theNurb);
+
+		gluNurbsSurface(theNurb, 
+						25,	knots,
+						25, knots,
+						21 * 3,
+						3,
+						&ctlpoints[0][0][0],
+						4, 4,
+						GL_MAP2_VERTEX_3);
+	/*
+
+		No cambien los numeros de la funcion,
+		solo deben de poner los nombres de las variables correspondiente.
+		
+	*/
+
+
+		gluEndSurface(theNurb);
+	
+	
+	/* Muestra los puntos de control */
+	
+	/*	int i,j;
+		glPointSize(5.0);
+		glDisable(GL_LIGHTING);
+		glColor3f(1.0, 1.0, 0.0);
+		glBegin(GL_POINTS);
+		for (i = 0; i <21; i++) {
+			for (j = 0; j < 21; j++) {
+	            glVertex3f(ctlpoints[i][j][0], 	ctlpoints[i][j][1], ctlpoints[i][j][2]);
+			}
+		}
+		glEnd();*/
+		glEnable(GL_LIGHTING);
+}
+
+void render(){
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+
+	GLfloat zExtent, xExtent, xLocal, zLocal;
+    int loopX, loopZ;
+
+	glLoadIdentity ();                       
+	gluLookAt (25.0, 12.0, 4.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	
+
+	// Luz y material
+
+	GLfloat mat_diffuse[] = { 0.6, 0.6, 0.9, 1.0 };
+	GLfloat mat_specular[] = { 0.8, 0.8, 1.0, 1.0 };
+	GLfloat mat_shininess[] = { 60.0 };
+	
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+	
+
+    GLfloat light_ambient[] = { 0.0, 0.0, 0.2, 1.0 };
+	GLfloat light_diffuse[] = { 0.8, 0.8, 0.8, 1.0 };
+	GLfloat light_specular[] = { 0.6, 0.6, 0.6, 1.0 };
+	GLfloat light_position[] = { -10.0, 5.0, 0.0, 1.0 };
+
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);   
+
+	// Render Grid 
+
+	//glDisable(GL_LIGHTING);
+	//glLineWidth(1.0);
+	//glPushMatrix();
+	//	glRotatef(angle,1.0,0.0,0.0);
+	//	glColor3f( 0.0, 0.7, 0.7 );
+	//	glBegin( GL_LINES );
+	//		zExtent = DEF_floorGridScale * DEF_floorGridZSteps;
+	//		for(loopX = -DEF_floorGridXSteps; loopX <= DEF_floorGridXSteps; loopX++ )
+	//		{
+	//			xLocal = DEF_floorGridScale * loopX;
+	//			glVertex3f( xLocal, -zExtent, 0.0 );
+	//			glVertex3f( xLocal, zExtent,  0.0 );
+	//		}
+	//		xExtent = DEF_floorGridScale * DEF_floorGridXSteps;
+	//	    for(loopZ = -DEF_floorGridZSteps; loopZ <= DEF_floorGridZSteps; loopZ++ )
+	//		{
+	//			zLocal = DEF_floorGridScale * loopZ;
+	//			glVertex3f( -xExtent, zLocal, 0.0 );
+	//			glVertex3f(  xExtent, zLocal, 0.0 );
+	//		}
+	//	glEnd();
+	//	ejesCoordenada();
+ //   glPopMatrix();
+
+	//glEnable(GL_LIGHTING);
+
+	// Fin Grid
+	
+
+
+	//Suaviza las lineas
+	glEnable(GL_BLEND); glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable( GL_LINE_SMOOTH );	
+
+	
+	glPushMatrix();
+		glRotatef(angle,-1.0,0.0,0.0);
+		//glTranslated(0.0,0.0,Zpos);
+		curva();
+	
+	glPopMatrix();
+
+	glDisable(GL_BLEND);
+	glDisable(GL_LINE_SMOOTH);
+
+	glutSwapBuffers();
+}
+
+void animacion(int value) {
+	
+	init_surface();
+	t += 0.1;
+	glutTimerFunc(10,animacion,1);
+    glutPostRedisplay();
+	
 }
 
 int main (int argc, char** argv) {
-	//Init GLUT & create windows
+
 	glutInit(&argc, argv);
+
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-	glutInitWindowSize(600,600);
-	glutInitWindowPosition (500, 100);
-	glutCreateWindow("Ola Circular");
 
+	glutInitWindowSize(960,540);
+
+	glutCreateWindow("Test Opengl");
+
+	init ();
+
+	glutReshapeFunc(changeViewport);
 	glutDisplayFunc(render);
-	//glutReshapeFunc(changeViewport);
-
+	glutKeyboardFunc(Keyboard);
+	glutSpecialFunc(specialKey);
+		
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		fprintf(stderr, "GLEW error");
+		return 1;
+	}
+	
+	glutTimerFunc(10,animacion,1);
 	glutMainLoop();
 	return 0;
+
 }
